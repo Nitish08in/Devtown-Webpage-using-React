a9d6ae939b23bb7f2a968285eade8197c911081c
Nitish Kumar Mahto <133137912+Nitish08in@users.noreply.github.com>
2023-05-11 03:19:38 +0530

    Add files via upload

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React JS Bootcamp</h1>
+      <p>
+        We learnt the basics of frontend web development, javascript, react,
+        react js, ReactDOM and Created a project at the very end of the bootcamp
+        as well.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
